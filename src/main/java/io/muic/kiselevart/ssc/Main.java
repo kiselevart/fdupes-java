7e37c73b2bc0cbfddaf18ff70207722cc29cdb9b
kiselevart <kiselevart1403@gmail.com>
2024-04-26 15:57:12 +0700

    update gitignore

--- src/main/java/io/muic/kiselevart/ssc/Main.java
+++ src/main/java/io/muic/kiselevart/ssc/Main.java
@@ -1,7 +1,18 @@
 package io.muic.kiselevart.ssc;
 
-public class Main {
+import java.nio.file.Files;
+import io.muzoo.ssc.assignment.tracker.SscAssignment;
+
+public class Main  extends SscAssignment  {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dir = System.getProperty("user.dir");
+        System.out.println(dir);
+
+        System.out.println("Number of arguments: " + args.length);
+        System.out.println("Arguments:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println((i + 1) + ": " + args[i]);
+        }
+        //Files.walkFileTree(null, null);
     }
 }
