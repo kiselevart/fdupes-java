7613f79c8edb0b72bdcb1f2c3e4c745113ece586
kiselevart <kiselevart1403@gmail.com>
2024-04-27 15:30:04 +0700

    made a recursive file counter

--- src/main/java/io/muic/kiselevart/ssc/Main.java
+++ src/main/java/io/muic/kiselevart/ssc/Main.java
@@ -1,18 +1,39 @@
 package io.muic.kiselevart.ssc;
 
-import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
 import io.muzoo.ssc.assignment.tracker.SscAssignment;
 
-public class Main  extends SscAssignment  {
-    public static void main(String[] args) {
-        String dir = System.getProperty("user.dir");
-        System.out.println(dir);
 
-        System.out.println("Number of arguments: " + args.length);
-        System.out.println("Arguments:");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println((i + 1) + ": " + args[i]);
+public class Main extends SscAssignment {
+    public static void main(String[] args) {
+        Path path = Paths.get("/home/artem/ssc/java-docs");
+        final int[] fileCount = {0};
+
+        try {
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    System.out.println(file.toAbsolutePath());
+                    fileCount[0]++;
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                    System.err.println("Failed to access: " + file.toString() + " due to " + exc.getMessage());
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            System.err.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
         }
-        //Files.walkFileTree(null, null);
+        
+        System.out.println(fileCount[0]);
     }
 }
+
+
+
