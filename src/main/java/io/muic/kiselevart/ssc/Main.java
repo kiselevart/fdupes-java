a3ee9bd408274990b753c50978a94330df354b7d
kiselevart <kiselevart1403@gmail.com>
2024-04-29 12:47:39 +0700

    removed .vscode, work on apache cli

--- src/main/java/io/muic/kiselevart/ssc/Main.java
+++ src/main/java/io/muic/kiselevart/ssc/Main.java
@@ -4,13 +4,31 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.*;
 import io.muzoo.ssc.assignment.tracker.SscAssignment;
+import org.apache.commons.cli.*;
 
 
 public class Main extends SscAssignment {
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Enter a path");
-            System.exit(1);
+        Options options = new Options();
+        options.addOption("c", "count-duplicates", false, "prints the total count of duplicate files");
+        options.addOption("a", "algorithm", true, "specifies the algorithm used");
+        options.addOption("p", "print", false, "prints relative paths of all duplicates grouped together");
+        options.addOption("f", true, "specifies path to folder, must be provided.");
+
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmd = parser.parse(options, args);
+            if (cmd.hasOption("f")) {
+                System.out.println("good");
+            }
+            else {
+                throw new ParseException("File path -f is required.");
+            }
+        }
+        catch (ParseException e) {
+            System.err.println("Error: " + e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("myapp", options);
         }
 
         String pathArg = args[0];
